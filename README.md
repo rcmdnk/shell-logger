69a31cc5a13b71a60b1a6a2c4834b58dd267e925
rcmdnk <rcmdnk@gmail.com>
2017-01-23 19:14:29 +0900

    Added license, description in the script. Updated format.

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
 # shell-logger
-Logger for shell script
+
+Logger for shell script.
+
+This includes functions of `debug`, `info`, `notice`, `warning` and `error`.
+Each output is formatted with date-time and colored by each color definition.
+
+These color codes are removed when the output is passed to a pipe or written into files.
+
+# Installation
+
+On Mac, you can install **etc/shell-logger.sh** by Homebrew:
+
+    $ brew tap rcmdnk/rcmdnkpac/shell-logger
+
+The file will be installed in **$(brew --prefix)/etc** (normally **/usr/local/etc**).
+
+Otherwise download shell-logger.sh and place it where you like.
+
+# Usage
+
+In your script, source shell-logger:
+
+    source /usr/local/etc/shell-logger.sh
+
+Then, you can use such `info` or `err` command in your script like:
+
+    somecommand
+    ret=$?
+    if [ ret = 0 ];then
+      info This is information
+    else
+      err Someting wrong!
+    fi
+
+Each level has each functions:
+
+LEVEL|Functions
+:----|:--------
+DEBUG|`debug`
+INFO|`info`, `information`
+NOTICE|`notice`, `notification`
+WARNING|`warn`, `warning`
+ERROR|`err`, `error`
+
+# Options
+
+Variable Name|Description|Default
+:------------|:----------|:-----
+_LOGGER_DATE_FORMAT|Output date format.|'%Y/%m/%d %H:%M:%S'
+_LOGGER_LEVEL|0: DEBUG, 1: INFO, 2: NOTICE, 3: WARN, 4: ERROR|1
+_LOGGER_DEBUG_COLOR|Color for DEBUG|3 (Italicized. Some terminal shows it as color inversion)
+_LOGGER_INFO_COLOR|Color for INFO|"" (Use default output color)
+_LOGGER_NOTICE_COLOR|Color for NOTICE|36 (Front color cyan)
+_LOGGER_WARNING_COLOR|Color for WARNING|33 (Front color yellow)
+_LOGGER_ERROR_COLOR|Color for ERROR|31 (Front color red)
+_LOGGER_ALWAYS_COLOR|Color mode. -1: Always no color. 0: Put color only for terminal output. 1: Always put color.|0
+_LOGGER_LEVELS|Names printed for each level. Need 5 names.|("DEBUG" "INFO" "NOTICE" "WARNING" "ERROR")
+
+About colors, you can find the standard definitions in
+[Standard ECMA-48](http://www.ecma-international.org/publications/standards/Ecma-048.htm)
+(p61, p62).
+
+Normal are:
+
+Number|Color definition
+:-----|:---------------
+30|black display
+31|red display
+32|green display
+33|yellow display
+34|blue display
+35|magenta display
+36|cyan display
+37|white display
+40|black background
+41|red background
+42|green background
+43|yellow background
+44|blue background
+45|magenta background
+46|cyan background
+47|white background
+
+You can set display (letter's color) and background in the same time.
+For example, if you want to use red background and white front color for error output,
+set:
+
+    _LOGGER_ERROR_COLOR="37;41"
+
---
+++ etc/shell-logger.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+## Description {{{
+#
+# Logger for shell script.
+#
+# Homepage: https://github.com/rcmdnk/shell-logger
+#
+_LOGGER_NAME="shell-logger"
+_LOGGER_VERSION="v0.0.1"
+_LOGGER_DATE="23/Jan/2017"
+# }}}
+
+## License {{{
+#
+#MIT License
+#
+#Copyright (c) 2017 rcmdnk
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+# }}}
+
+# Default variables {{{
+_LOGGER_DATE_FORMAT=${_LOGGER_DATE_FORMAT:-'%Y/%m/%d %H:%M:%S'}
+_LOGGER_LEVEL=${_LOGGER_LEVEL:-1} # 0: debug, 1: info, 2: notice, 3: warning, 4: error
+_LOGGER_DEBUG_COLOR=${_LOGGER_INFO_COLOR:-"3"}
+_LOGGER_INFO_COLOR=${_LOGGER_INFO_COLOR:-""}
+_LOGGER_NOTICE_COLOR=${_LOGGER_INFO_COLOR:-"36"}
+_LOGGER_WARNING_COLOR=${_LOGGER_INFO_COLOR:-"33"}
+_LOGGER_ERROR_COLOR=${_LOGGER_INFO_COLOR:-"31"}
+_LOGGER_ALWAYS_COLOR=${_LOGGER_INFO_COLOR:-0}
+if [ "${_LOGGER_LEVELS}" = "" ];then
+  _LOGGER_LEVELS=("DEBUG" "INFO" "NOTICE" "WARNING" "ERROR")
+fi
+# }}}
+
+# Functions {{{
+function _logger_version () {
+  printf "%s %s %s\n" "$_LOGGER_NAME" "$_LOGGER_VERSION" "$_LOGGER_DATE"
+}
+function _logger_time () {
+  printf "[$(date +"$_LOGGER_DATE_FORMAT")] %s"  "$*"
+}
+function _logger () {
+  local logger_level=$_LOGGER_LEVEL
+  expr "$_LOGGER_LEVEL" + 1 >/dev/null 2>&1
+  if [ $? -ge 2 ];then
+    logger_level=0
+    local n=0
+    for l in "${_LOGGER_LEVELS[@]}";do
+      if [ "$_LOGGER_LEVEL" = "$l" ];then
+        logger_level=$n
+        break
+      fi
+      ((n++))
+    done
+  fi
+  local level=$1
+  shift
+  if [ "$logger_level" -gt "$level" ];then
+    return
+  fi
+  [ -z "$ZSH_VERSION" ] || emulate -L ksh
+  local msg
+  msg=$(_logger_time "[${_LOGGER_LEVELS[$level]}]: $*")
+  local log_colors=("$_LOGGER_DEBUG_COLOR" "$_LOGGER_INFO_COLOR" "$_LOGGER_NOTICE_COLOR" "$_LOGGER_WARNING_COLOR" "$_LOGGER_ERROR_COLOR")
+  if [ "$_LOGGER_ALWAYS_COLOR" != -1 ] && ([ -t 1 ] || [ "$_LOGGER_ALWAYS_COLOR" = 1 ]);then
+    printf "\e[${log_colors[$level]}m%s\e[m\n"  "$msg"
+  else
+    printf "%s\n" "$msg"
+  fi
+}
+debug () {
+  _logger 0 "$*"
+}
+info () {
+  _logger 1 "$*"
+}
+alias information=info
+notice () {
+  _logger 2 "$*"
+}
+alias notification=notice
+warn () {
+  _logger 3 "$*"
+}
+alias warning=warn
+err () {
+  _logger 4 "$*"
+}
+alias error=err
+# }}}
